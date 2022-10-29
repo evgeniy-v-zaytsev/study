d949038ba7cde22d4c9ae0178c9c7d04cb7be18d
evgeniy-v-zaytsev <93654705+evgeniy-v-zaytsev@users.noreply.github.com>
2022-10-29 16:08:31 +0300

    execHivecmd

--- 3/3.8.1/result/execHiveCmd.sh
+++ 3/3.8.1/result/execHiveCmd.sh
@@ -3,17 +3,20 @@ sqlInput=$1
 
 logFile="exec_stdout.log"
 errFile="exec_stderr.log"
+execFile="/tmp/zaytsevev/sql.txt"
 [ -f $logFile ] && rm $logFile
 [ -f $errFile ] && rm $errFile
+[ -f $execFile ] && rm $execFile
 
 sqlCmd="!connect  jdbc:hive2://10.93.1.9:10000 hive eee;
 "$sqlInput";"
 
-echo $sqlCmd
+echo $sqlCmd >$execFile
 
-result=$(beeline -f $sqlCmd) >$logFile 2>$errFile
-if [ ! $? -eq 0 ]; then 
-	echo "There were errors while working with dir, check "$errFile
+result=$(beeline -f $execFile) >$logFile 2>$errFile
+r=$?
+if [ ! $r -eq 0 ]; then 
+	echo "There were errors while working with dir, check $errFile"
 else
 	echo $result
 fi
